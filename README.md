08bf6a3263554e47df4a1602bbfc4ad991af1577
Matan Kushner <hello@matchai.me>
2018-11-17 15:28:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# spacefish-bot
+
+> 🤖 A helpful bot to automate common tasks on [spacefish](https://github.com/matchai/spacefish) Issues/PRs
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how spacefish-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2018 Matan Kushner &lt;hello@matchai.me&gt;
---
+++ index.js
@@ -0,0 +1,40 @@
+const formatIssueClone = require("./lib/format-issue-clone.js");
+
+/**
+ * This is the entry point for your Probot App.
+ * @param {import('probot').Application} app - Probot's Application class.
+ */
+module.exports = app => {
+  app.log("Yay, spacefish-bot was loaded!");
+
+  app.on(["issues.opened", "issues.edited"], async context => {
+    const { title } = context.payload.issue;
+    if (!isIssueNumber(title)) return;
+
+    const issueNumber = getIssueNumber(title);
+    context.log(`Issue #${issueNumber} is being cloned`);
+
+    const spaceshipIssue = await getSpaceshipIssue({ context, issueNumber });
+    context.log(
+      `Issue found: #${spaceshipIssue.data.number} ${spaceshipIssue.data.title}`
+    );
+
+    const formattedIssue = formatIssueClone({ context, issue: spaceshipIssue });
+    return context.github.issues.edit(formattedIssue);
+  });
+};
+
+const getIssueNumber = str => {
+  const regex = /#(\d+)/.exec(str);
+  return regex ? regex[1] : null;
+};
+const isIssueNumber = str => Boolean(getIssueNumber(str));
+const getSpaceshipIssue = async ({ context, issueNumber }) => {
+  const test = context.github.issues.get({
+    owner: "denysdovhan",
+    repo: "spaceship-prompt",
+    number: issueNumber
+  });
+  console.log(test);
+  return test;
+};
